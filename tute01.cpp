61c66ec7af723b436fc6e144bac10098de204b88
I.U.N.Nadeeshan <89896805+it21166556@users.noreply.github.com>
2022-02-21 19:07:18 +0000

    I have removed some comments

--- tute01.cpp
+++ tute01.cpp
@@ -22,9 +22,12 @@ printf("Hello World") --> std::cout << "Hello World";
 int main() 
 {
     float cm, inches;
-    std::cout << "Enter a length in cm : ";//printf("Enter a length in cm : ");
-    std::cin >> cm;//scanf("%f",&cm);
+  
+    std::cout << "Enter a length in cm : ";
+    std::cin >> cm;
+  
     inches = cm / 2.54;
-    std::cout << "Length in inches is " << inches << std:: endl;//printf("Length in inches is %f \n", inches);
+  
+    std::cout << "Length in inches is " << inches << std:: endl;
     return 0;
 }  
