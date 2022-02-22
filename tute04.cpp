7e5960d2b9e774cb5060c3ee4443dc0076789b60
I.U.N.Nadeeshan <89896805+it21166556@users.noreply.github.com>
2022-02-22 18:59:27 +0000

    I have completed tute 04.cpp

--- tute04.cpp
+++ tute04.cpp
@@ -27,3 +27,19 @@ int main() {
   std::cout << std::endl;
   return 0;
 }
+long Factorial(int no)
+{
+  int i,fac = 1;
+  for(i = 1 ; i <= no ; ++i)
+    {
+      fac = fac * i;
+    }
+    return fac;
+}
+long nCr(int n, int r)
+{
+  int fac;
+  fac =  Factorial(n) / Factorial(r) * Factorial(n-r);
+  return fac; 
+  
+}
